313c3ecaea349edf332b58a81929012b694e3871
MrHiTech <mlebowitz97@gmail.com>
2024-09-22 03:14:46 -0400

    First commit

---
+++ src/main/java/net/mrhitech/purified_water/item/PurifiedWaterItems.java
@@ -0,0 +1,18 @@
+package net.mrhitech.purified_water.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.mrhitech.purified_water.PurifiedWater;
+
+public class PurifiedWaterItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, PurifiedWater.MOD_ID);
+
+
+
+    public static void register(IEventBus bus) {
+        ITEMS.register(bus);
+    }
+
+}
